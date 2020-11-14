98cd567816ce1e58d33b1d6ff4ac5f13cce0f490
Peter Kim <petekim@gmail.com>
2020-11-14 09:16:11 -0800

    Create payload4-time.js

---
+++ payload4-time.js
@@ -0,0 +1,13 @@
+{
+  "option": "time"
+}
+
+Displays an error. Must also specify a 'period' of 'yesterday', 'today', or 'tomorrow'.
+{
+  "option": "date"
+}
+
+Displays an error. Must specify an 'option' of 'date' or 'time'.
+{
+  "option": "dates"
+}
